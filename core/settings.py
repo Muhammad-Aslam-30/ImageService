cbcf0f111cdd9f424b1d960f0792f470ae49d56b
Muhammad-Aslam-30 <102525782+Muhammad-Aslam-30@users.noreply.github.com>
2023-11-27 14:21:19 +0100

    Update DB credentials

--- core/settings.py
+++ core/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'djangodb',
-        'USER': 'postgres',
-        'PASSWORD': 'recipespw',
+        'USER': '*****',
+        'PASSWORD': '*****',
         'HOST': 'localhost',
         'PORT': '5432',
     }
